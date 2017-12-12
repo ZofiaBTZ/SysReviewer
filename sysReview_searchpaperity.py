eb603000cd37b8698a22c85bd3ffade2c993da06
Zofia Baranczuk <zofia.baranczuk@math.uzh.ch>
2017-12-12 11:54:01 +0100

    Change the name to words_coocurences_paperity.py

--- sysReview_searchpaperity.py
+++ sysReview_searchpaperity.py
@@ -7,11 +7,6 @@ import argparse
 import webbrowser, datetime
 
 
-
-
-parser = argparse.ArgumentParser(description= "Articles search arguments", add_help=True)
-group = parser.add_mutually_exclusive_group()
-
 #Define validity of search period /date
 def valid_date(s):
     try:
@@ -22,48 +17,57 @@ def valid_date(s):
         raise argparse.ArgumentTypeError(msg)
 
 #Add all necessary arguments
-parser.add_argument("--startdate", "-sd",
-                    help="Search start date - format",
-                                     type=valid_date, dest="startdate")
 
-parser.add_argument("--enddate", "-ed",
-                    help="Search end date - format",
-                                     type=valid_date, dest="enddate")
+def main():
+    parser = argparse.ArgumentParser(description= "Articles search arguments", add_help=True)
+    group = parser.add_mutually_exclusive_group()
 
-parser.add_argument('--author', '-a', required=True,default= "", help='Author',action="store", dest= "author")
-parser.add_argument('--pagenumber', '-p',default= 1,type=int,help='Page number',action="store", dest= "pagenumber")
-parser.add_argument('--keywords', '-k', required=True, help='Search keyword', action="store", dest="keyW")
-#parser.add_argument('--journal', '-j',help='Journal name', action="store", dest= "journal", \
-                        #choices= ["PLOS ONE","PLOS Medicine","BMC Public Health","BMC Infectious Diseases","BMC Medicine", "SpringerPlus"])
-parser.add_argument("--output", "-o",default=True, help="Output the " + \
-                    "query to a file", action="store_true", dest="Output")
+    parser.add_argument("--startdate", "-sd",
+                        help="Search start date - format",
+                        type=valid_date, dest="startdate")
 
-group.add_argument('--quiet','-q', action='store_true', help='print quiet')
-group.add_argument('--verbose','-v', default=True,action='store_true', help='print verbose')
+    parser.add_argument("--enddate", "-ed",
+                        help="Search end date - format",
+                        type=valid_date, dest="enddate")
 
-#Printing the parsed arguments
-print parser.parse_args()
-args = parser.parse_args()
-print "Articles published from %s  to %s" % (args.startdate, args.enddate)
-print "By :%s " % (args.author)
-print "Keywords :%s " % args.keyW
-print "Results page number :%s " % args.pagenumber
-print "Query saved? =%s " % args.Output
+    parser.add_argument('--author', '-a', required=True,default= "", help='Author',action="store", dest= "author")
+    parser.add_argument('--pagenumber', '-p',default= 1,type=int,help='Page number',action="store", dest= "pagenumber")
+    parser.add_argument('--keywords', '-k', required=True, help='Search keyword', action="store", dest="keyW")
+    #parser.add_argument('--journal', '-j',help='Journal name', action="store", dest= "journal", \
+        #choices= ["PLOS ONE","PLOS Medicine","BMC Public Health","BMC Infectious Diseases","BMC Medicine", "SpringerPlus"])
+    parser.add_argument("--output", "-o",default=True, help="Output the " + \
+                        "query to a file", action="store_true", dest="Output")
 
-#Checking if requred and necessary arguments are parsed
-if args.author or args.keyW or args.startdate or args.enddate:
-    url = "http://paperity.org/search/" + str(args.pagenumber) + "?as_c="
-    query = url + args.keyW + "&as_au=" + args.author \
-    + "&f_date_start=" + str(args.startdate) + "&f_date_end=" + str(args.enddate) 
+    group.add_argument('--quiet','-q', action='store_true', help='print quiet')
+    group.add_argument('--verbose','-v', default=True,action='store_true', help='print verbose')
 
-else :
-    print("Incomplete search. Please enter required information : Author's name and Keywords")
+    #Printing the parsed arguments
+    print parser.parse_args()
+    args = parser.parse_args()
+    print "Articles published from %s  to %s" % (args.startdate, args.enddate)
+    print "By :%s " % (args.author)
+    print "Keywords :%s " % args.keyW
+    print "Results page number :%s " % args.pagenumber
+    print "Query saved? =%s " % args.Output
 
-#Writing the queries to a text file
-if  args.Output :
-    f = open("query.txt","a")
-    f.write(str(query) + '\n')
+    #Checking if requred and necessary arguments are parsed
+    if args.author or args.keyW or args.startdate or args.enddate:
+        url = "http://paperity.org/search/" + str(args.pagenumber) + "?as_c="
+        query = url + args.keyW + "&as_au=" + args.author \
+                + "&f_date_start=" + str(args.startdate) + "&f_date_end=" + str(args.enddate) 
 
+    else :
+        print("Incomplete search. Please enter required information : Author's name and Keywords")
+        query = ""
+        
+    #Writing the queries to a text file
+    #TODO:  do it outside this function, in the folder of this search 
+    #if  args.Output :
+    #    f = open("query.txt","a")
+    #    f.write(str(query) + '\n')
+    return(query)
+
+    
 #Requesting for quite or verbose execution
 if __name__ == "__main__":
     if args.quiet :
